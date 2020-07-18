517080bad975474c52246c26b3b9ca1c70629d55
Jaime van Kessel <nallath@gmail.com>
2020-07-18 16:19:43 +0200

    Move Preload to own file

--- gui.cpp
+++ gui.cpp
@@ -25,6 +25,7 @@ Created by Lewis he on October 10, 2019.
 #include "Keyboard.h"
 #include "Switch.h"
 #include "Task.h"
+#include "Preload.h"
 
 #define RTC_TIME_ZONE   "CST+7"
 
@@ -328,83 +329,9 @@ const char *Keyboard::btnm_mapplus[10][23] = {
 
 
 
-
 Switch *Switch::_switch = nullptr;
 
-
-/*****************************************************************
- *
- *          ! Preload Class
- *
- */
-class Preload
-{
-public:
-    Preload()
-    {
-        _preloadCont = nullptr;
-    }
-    ~Preload()
-    {
-        if (_preloadCont == nullptr) return;
-        lv_obj_del(_preloadCont);
-        _preloadCont = nullptr;
-    }
-    void create(lv_obj_t *parent = nullptr)
-    {
-        if (parent == nullptr) {
-            parent = lv_scr_act();
-        }
-        if (_preloadCont == nullptr) {
-            static lv_style_t plStyle;
-            lv_style_init(&plStyle);
-            lv_style_set_radius(&plStyle, LV_OBJ_PART_MAIN, 0);
-            lv_style_set_bg_color(&plStyle, LV_OBJ_PART_MAIN, LV_COLOR_GRAY);
-            lv_style_set_bg_opa(&plStyle, LV_OBJ_PART_MAIN, LV_OPA_0);
-            lv_style_set_border_width(&plStyle, LV_OBJ_PART_MAIN, 0);
-            lv_style_set_text_color(&plStyle, LV_OBJ_PART_MAIN, LV_COLOR_WHITE);
-            lv_style_set_image_recolor(&plStyle, LV_OBJ_PART_MAIN, LV_COLOR_WHITE);
-
-
-            static lv_style_t style;
-            lv_style_init(&style);
-            lv_style_set_radius(&style, LV_OBJ_PART_MAIN, 0);
-            lv_style_set_bg_color(&style, LV_OBJ_PART_MAIN, LV_COLOR_GRAY);
-            lv_style_set_bg_opa(&style, LV_OBJ_PART_MAIN, LV_OPA_0);
-            lv_style_set_border_width(&style, LV_OBJ_PART_MAIN, 0);
-            lv_style_set_text_color(&style, LV_OBJ_PART_MAIN, LV_COLOR_WHITE);
-            lv_style_set_image_recolor(&style, LV_OBJ_PART_MAIN, LV_COLOR_WHITE);
-
-            _preloadCont = lv_cont_create(parent, NULL);
-            lv_obj_set_size(_preloadCont, LV_HOR_RES, LV_VER_RES - 30);
-            lv_obj_align(_preloadCont, NULL, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
-            lv_obj_add_style(_preloadCont, LV_OBJ_PART_MAIN, &plStyle);
-
-            lv_obj_t *preload = lv_spinner_create(_preloadCont, NULL);
-            lv_obj_set_size(preload, lv_obj_get_width(_preloadCont) / 2, lv_obj_get_height(_preloadCont) / 2);
-            lv_obj_add_style(preload, LV_OBJ_PART_MAIN, &style);
-            lv_obj_align(preload, _preloadCont, LV_ALIGN_CENTER, 0, 0);
-        }
-    }
-    void align(const lv_obj_t *base, lv_align_t align, lv_coord_t x = 0, lv_coord_t y = 0)
-    {
-        lv_obj_align(_preloadCont, base, align, x, y);
-    }
-
-    void hidden(bool en = true)
-    {
-        lv_obj_set_hidden(_preloadCont, en);
-    }
-
-private:
-    lv_obj_t *_preloadCont = nullptr;
-};
-
-
-
 List *List::_list = nullptr;
-
-
 
 
 /*****************************************************************
